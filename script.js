87bbfbc6694feff8545f56a2de39d9aa9cf803fc
OlivaresY <124736762+OlivaresY@users.noreply.github.com>
2026-03-10 14:53:29 -0600

    agregando libro - if -else existencia

--- script.js
+++ script.js
@@ -7,15 +7,25 @@ class Libro {
         this.disponible = true;
     }
 
-    info() { //se define como se crea y como se muestra el libro
+    info() { //definiendo como se crea y como se mostrará el libro
         return `${this.titulo} de ${this.autor} (${this.anio}) - ${this.disponible ? "Disponible" : "Prestado"}`;
     }
 }
 
 class Biblioteca {
     constructor(nombre) {
-        //Nombre -> string
-        //liros -> arreglo vacio
+        this.nombre = nombre;
+        this.libros = [];
+    }
+
+    agregarLibro(libro) {
+        const existe = this.libros.find(l => l-titulo.toLowerCase() === libro.titulo.toLowerCase());
+        if (existe) {
+            console.error(`El libro "${libro.titutlo}" ya existe en la biblioteca.`);
+        } else {
+            this.libros.push(libro);
+            console.log(`Libro "${libro.titulo}" agregado correctamente.`);
+        }
     }
 }
 
