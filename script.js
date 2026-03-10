764bafb33d0646d7ab9f083d93cef3f0ddf2eead
OlivaresY <124736762+OlivaresY@users.noreply.github.com>
2026-03-10 13:17:59 -0600

    creando la estructura HTML y el constructor de Libro

---
+++ script.js
@@ -0,0 +1,13 @@
+class Libro {
+    constructor(Titulo, autor, genero, anio) {
+        //Titulo -> String
+        //autor -> string
+        //genero -> string(ej: "Ficcion", #"Ciencia")
+        //anio -> number
+        //disponible -> boolean, inicia en true
+    }
+
+    instanceof() {
+        //retorna: "[Titulo de [Autor] ([Año]) - Disponible / Prestado"
+    }
+}
